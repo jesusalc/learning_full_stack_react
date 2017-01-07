aae7ca4597d04d8e8f5b65f29bd43d0f9cc50fc4
jesusalc <jesusalc@gmail.com>
2017-01-07 16:17:06 +0100

    added className ans browserify fix

--- app.js
+++ app.js
@@ -1,3 +1,3 @@
-const h1 = React.createElement('h1', null, "Hell world");
+const h1 = React.createElement('h1', {className:'success'}, "Hello world");
 
 ReactDOM.render(h1, document.getElementById('app'))
