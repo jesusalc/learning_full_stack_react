656366dad2e88b0403d4d6e8d530a18bb87771d4
jesusalc <jesusalc@gmail.com>
2017-01-07 17:07:34 +0100

    added first class ejecuting

--- src/app.js
+++ src/app.js
@@ -1,4 +1,20 @@
 const React = require('react')
 const ReactDOM = require('react-dom')
 
-ReactDOM.render(<h1  className="success" htmlFor="" >Hello World!</h1>, document.getElementById('app'))
+const Layout = React.createClass({
+    render: function(){
+        return <div class="overlay">
+            <div class="box">
+                <h2>Title</h2>
+               
+            </div>
+            <footer>By signing up, you are agreeing to our 
+                <a href="/terms"> Terms of Service </a> 
+                and to our
+                <a href="/privacy"> Privacy Policy </a> 
+            </footer>
+        </div>
+    }
+})
+
+ReactDOM.render(<Layout />, document.getElementById('app'))
