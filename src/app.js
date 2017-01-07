57a27c16f301a9550ec61f4a73100495b932815d
jesusalc <jesusalc@gmail.com>
2017-01-07 17:22:46 +0100

    added first children class

--- src/app.js
+++ src/app.js
@@ -6,7 +6,7 @@ const Layout = React.createClass({
         return <div className="overlay">
             <div className="box">
                 <h2>{this.props.title}</h2>
-               
+                {this.props.children}
             </div>
             <footer>By signing up, you are agreeing to our 
                 <a href="/terms"> Terms of Service </a> 
@@ -17,4 +17,27 @@ const Layout = React.createClass({
     }
 })
 
-ReactDOM.render(<Layout title="Login" />, document.getElementById('app'))
+const Login = React.createClass({
+    render: function(){
+        return                 <form method="post" accept-charset="utf-8" action="/u/login">
+                    <input type="hidden" name="_method" value="POST"  />
+                    <input type="email" name="email" required="required" placeholder="Password" id="password"  />
+                    <input type="hidden" name="remember_me" value="0" />
+                    <p>
+                        <label htmlFor="remember-me">
+                            <input type="checkbox" name="remember-me" value="1" checked="checked" id="remember-me" />
+                            Remember me
+                        </label>
+                    </p>
+                    <a href="register.html" className="inlink" >Register</a>
+                    <a href="#request-reset-password" className="inlink" >Reset Password</a>
+                    <button type="submit" >Login</button>
+                </form>
+
+    }
+})
+
+
+ReactDOM.render(<Layout title="Login" >
+  <Login></Login>
+</Layout> , document.getElementById('app'))
