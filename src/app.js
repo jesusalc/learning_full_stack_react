b4efd9b9d43248ba068444c23021a7a70b68f286
jesusalc <jesusalc@gmail.com>
2017-01-07 18:15:00 +0100

    upgraded forms inputs with material ui

--- src/app.js
+++ src/app.js
@@ -3,6 +3,11 @@ const ReactDOM = require('react-dom')
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import TextField from 'material-ui/TextField';
 import {orange500, blue500} from 'material-ui/styles/colors';
+import RaisedButton from 'material-ui/RaisedButton';
+
+const style = {
+  margin: 12,
+}
 
 const styles = {
   errorStyle: {
@@ -17,7 +22,7 @@ const styles = {
   floatingLabelFocusStyle: {
     color: blue500,
   },
-};
+}
 
 const Layout = React.createClass({
     propTypes:{
@@ -41,11 +46,48 @@ const Layout = React.createClass({
 
 const Email = React.createClass({
     render: function(){
-        return <input type="email" name="email" required data-info="An active email account is needed to gain access" title="An active email account is needed to gain access" pattern="^[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$" maxLength="255" placeholder="Your email" max id="email"  />
+        return <TextField 
+          type="email" 
+          name="email" 
+          required 
+          floatingLabelText="Your email" 
+          pattern="^[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$" 
+          maxLength="255" 
+          floatingLabelStyle={styles.floatingLabelStyle}
+          floatingLabelFocusStyle={styles.floatingLabelFocusStyle}
+          id="email"  />
     }
 })
 
+const Password = React.createClass({
+    render: function(){
+        return <TextField 
+          type="password" 
+          name="password" 
+          required 
+          id="password" 
+          floatingLabelText="Password" 
+          pattern="^[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$" 
+          maxLength="255" 
+          floatingLabelStyle={styles.floatingLabelStyle}
+          floatingLabelFocusStyle={styles.floatingLabelFocusStyle} />
+    }
+})
 
+const PasswordConfirm = React.createClass({
+    render: function(){
+        return <TextField 
+          type="password" 
+          name="password2" 
+          required 
+          id="password2" 
+          floatingLabelText="Password Confirm" 
+          pattern="^[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$" 
+          maxLength="255" 
+          floatingLabelStyle={styles.floatingLabelStyle}
+          floatingLabelFocusStyle={styles.floatingLabelFocusStyle} />
+    }
+})
 
 const Login = React.createClass({
     getInitialState: function() {
@@ -55,38 +97,38 @@ const Login = React.createClass({
 
     },
     render: function(){
-        return                 <form method="post" acceptCharset="utf-8" action="/u/login">
-                    <input type="hidden" name="_method" value="POST"  />
-                    <Email />
-                    <input type="password" name="password" required="required" placeholder="Password" id="password"  />
-                    <input type="hidden" name="remember_me" value="0" />
-                    <p>
-                        <label htmlFor="remember-me">
-                            <input type="checkbox" name="remember-me" value="1" defaultChecked onChange={this.OnChangeRemmber} id="remember-me" />
-                            Remember me
-                        </label>
-                    </p>
-                    <p>
-                      <a href="register.html" className="inlink" > Register </a>
-                      | <a href="#request-reset-password" className="inlink" > Reset Password </a>
-                    </p> 
-                    <RaisedButton label="Login" type="submit" primary style={style} />
-                </form>
+        return <form method="post" acceptCharset="utf-8" action="/u/login">
+            <input type="hidden" name="_method" value="POST"  />
+            <Email />
+            <Password />
+            
+            <input type="hidden" name="remember_me" value="0" />
+            <p>
+                <label htmlFor="remember-me">
+                    <input type="checkbox" name="remember-me" value="1" defaultChecked onChange={this.OnChangeRemmber} id="remember-me" />
+                    Remember me
+                </label>
+            </p>
+            <p>
+              <a href="register.html" className="inlink" > Register </a>
+              | <a href="#request-reset-password" className="inlink" > Reset Password </a>
+            </p> 
+            <RaisedButton label="Login" type="submit" primary style={style} />
+        </form>
 
     }
 })
 
 const Register = React.createClass({
     render: function(){
-        return                 <form method="post" acceptCharset="utf-8" action="/u/register">
-                    <input type="hidden" name="_method" value="POST"  />
-                    <Email />
-                    <input type="password" name="password" required="required" placeholder="Password" id="password"  />
-                    <input type="password" name="password2" required="required" placeholder="Password Confirm" id="password2"  />
-                    <span className="col-p100 info" ></span>
-                    <RaisedButton label="Register" type="submit" primary style={style} />
-                    
-                </form>
+        return <form method="post" acceptCharset="utf-8" action="/u/register">
+            <input type="hidden" name="_method" value="POST"  />
+            <Email />
+            <Password />
+            <PasswordConfirm />
+            <span className="col-p100 info" ></span>
+            <RaisedButton label="Register" type="submit" primary style={style} /> 
+        </form>
 
     }
 })
