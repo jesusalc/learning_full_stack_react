45b571953e2cda0a0ec1b06fec1a17ac16875478
jesusalc <jesusalc@gmail.com>
2017-01-07 17:38:29 +0100

    added register class

--- src/app.js
+++ src/app.js
@@ -41,7 +41,21 @@ const Login = React.createClass({
     }
 })
 
+const Register = React.createClass({
+    render: function(){
+        return                 <form method="post" accept-charset="utf-8" action="/u/register">
+                    <input type="hidden" name="_method" value="POST"  />
+                    <input type="email" name="email" required data-info="An active email account is needed to gain access" title="An active email account is needed to gain access" pattern="^[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$" maxLength="255" placeholder="Your email" max id="email"  />
+                    <input type="password" name="password" required="required" placeholder="Password" id="password"  />
+                    <input type="password" name="password2" required="required" placeholder="Password Confirm" id="password2"  />
+                    <span className="col-p100 info" ></span>
+                    <button type="submit" >Register</button>
+                </form>
+
+    }
+})
+
 
 ReactDOM.render(<Layout title="Login" >
-  <Login></Login>
+  <Register></Register>
 </Layout> , document.getElementById('app'))
