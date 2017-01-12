2d1a0cbed56b98efe013f8ab8aa36587a7b1a40c
jesusalc <jesusalc@gmail.com>
2017-01-12 10:47:09 +0100

    added Auth.js

---
+++ src/Auth.js
@@ -0,0 +1,181 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import ReactDOMServer from 'react-dom/server'
+import injectTapEventPlugin from 'react-tap-event-plugin';
+
+// Needed for onTouchTap
+// http://stackoverflow.com/a/34015469/988941
+injectTapEventPlugin();
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider'
+import TextField from 'material-ui/TextField'
+import { orange500, blue500 } from 'material-ui/styles/colors'
+import RaisedButton from 'material-ui/RaisedButton'
+
+const style = {
+  margin: 12,
+}
+
+const styles = {
+  errorStyle: {
+    color: orange500,
+  },
+  underlineStyle: {
+    borderColor: orange500,
+  },
+  floatingLabelStyle: {
+    color: orange500,
+  },
+  floatingLabelFocusStyle: {
+    color: blue500,
+  },
+}
+
+
+class Email extends React.Component {
+  render() {
+    return <TextField
+      type="email"
+      name="email"
+      key="email"
+      required
+      floatingLabelText="Your email"
+      pattern="^[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$"
+      maxLength="255"
+      floatingLabelStyle={styles.floatingLabelStyle}
+      floatingLabelFocusStyle={styles.floatingLabelFocusStyle}
+      id="email" />
+  }
+}
+
+
+class PasswordConfirm extends React.Component {
+  render() {
+    return <TextField
+      key="password2"
+      type="password"
+      name="password2"
+      required
+      id="password2"
+      floatingLabelText="Password Confirm"
+      pattern="^[a-z0-9._%+-]$"
+      maxLength="255"
+      floatingLabelStyle={styles.floatingLabelStyle}
+      floatingLabelFocusStyle={styles.floatingLabelFocusStyle} />
+  }
+}
+
+class Password extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {password1: '', password2: ''}
+    this.onChange = this.onChange.bind(this)
+  }
+  onChange({target}){
+    this.setState({[target.name]: target.value})
+  }
+  render() {
+    let ret = [<TextField
+      key="password"
+      type="password"
+      name="password1"
+      required
+      id="password"
+      floatingLabelText="Password"
+      pattern="^[a-z0-9._%+-]$"
+      maxLength="255"
+      value={this.state.password1}
+      onChange={this.onChange}
+      floatingLabelStyle={styles.floatingLabelStyle}
+      floatingLabelFocusStyle={styles.floatingLabelFocusStyle} />]
+
+    
+    if (this.props.msg) {
+      ret.push(<div className="error-message">{this.props.msg}</div>)
+    }  
+    if (this.props.confirm) {
+      ret[1] = <TextField
+      key="password2"
+      type="password"
+      name="password2"
+      required
+      id="password2"
+      floatingLabelText="Password Confirm"
+      pattern="^[a-z0-9._%+-]$"
+      maxLength="255"
+      value={this.state.password2}
+      onChange={this.onChange}
+      floatingLabelStyle={styles.floatingLabelStyle}
+      floatingLabelFocusStyle={styles.floatingLabelFocusStyle} />
+    }
+    if (this.state.password2.length && this.state.password1 != this.state.password2) {
+      msg = <div className="error-message">Both passwords must be the same</div>
+    }
+    return <div>{ret}{msg}</div>
+  }
+}
+
+
+class Login extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { remmber: true }
+    this.onChange = this.OnChangeRemmber.bind(this)
+  }
+  OnChangeRemmber(e) {
+
+  }
+  render() {
+    return <form method="post" acceptCharset="utf-8" action="/u/login">
+      <input type="hidden" name="_method" value="POST" />
+      <Email />
+      <Password />
+      <input type="hidden" name="remember_me" value="0" />
+      <p>
+        <label htmlFor="remember-me">
+          <input
+            type="checkbox"
+            name="remember-me"
+            value="1"
+            defaultChecked
+            onChange={this.OnChangeRemmber}
+            id="remember-me" />
+          Remember me
+              </label>
+      </p>
+      <p>
+        <a href="register.html" className="inlink" > Register </a>
+        | <a href="#request-reset-password" className="inlink" > Reset Password </a>
+      </p>
+      <RaisedButton label="Login" type="submit" primary style={style} />
+    </form>
+
+  }
+}
+
+class Register extends React.Component {
+  render() {
+    return <form method="post" acceptCharset="utf-8" action="/u/register">
+      <input type="hidden" name="_method" value="POST" />
+      <Email />
+      <Password confirm="1" msg={this.props.msg} />
+      <span className="col-p100 info" ></span>
+      <RaisedButton label="Register" type="submit" primary style={style} />
+    </form>
+
+  }
+}
+
+
+ReactDOM.render(<MuiThemeProvider>
+  <Layout title="Login" >
+    <Register></Register>
+    <Login></Login>
+  </Layout>
+</MuiThemeProvider>, document.getElementById('app'))
+
+
+module.exports = {
+  register(msg) {
+    return ReactDOMServer.renderToString(<Register msg={msg} />)
+  }
+}
